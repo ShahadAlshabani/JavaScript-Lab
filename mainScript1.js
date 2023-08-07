90bb2e113caafdfd426a5e7f993652df53c60e5d
shahadM121 <132405219+shahadM121@users.noreply.github.com>
2023-08-07 15:12:47 +0300

    Update mainScript1.js

--- mainScript1.js
+++ mainScript1.js
@@ -1,22 +1,23 @@
 //Shahad Alshabani
 
+console.log("========square number between 1 and 10 ==========");
 
 for(let i =1 ; i<= 10 ; i++){
     console.log(Math.pow(i,2));
 }
-console.log("==================");
+console.log("=========odd number between 1 and 20=========");
 
 for(let i =1 ; i<= 20 ; i++){
     if(i % 2 != 0)
     console.log(i);
 }
-console.log("==================");
+console.log("=========even number between 1 and 25.=========");
 
 for(let i =1 ; i<= 25 ; i++){
     if(i % 2 == 0)
     console.log(i);
 }
-console.log("==================");
+console.log("=========Tree of numbers=========");
 
 let output = "";
 for(let i =1 ; i<= 8 ; i++){
@@ -29,7 +30,7 @@ for(let i =1 ; i<= 8 ; i++){
 
    console.log(output);
 }
-console.log("==================");
+console.log("========Fibonacci sequence==========");
 
 
 let num1= 0;
